2ecb124ed0cd8123b3415111aafbe82469597d06
TANKominator5 <debajitpal.380718@gmail.com>
2025-07-06 12:20:39 +0530

    best practices

--- clinical-data-frontend/src/firebase.js
+++ clinical-data-frontend/src/firebase.js
@@ -2,14 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// 🚨 PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
+// Define each config value as a variable from environment
+const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+const FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID;
+
+// Use the variables in the config object
 const firebaseConfig = {
-  apiKey: "AIzaSyCrLo380Vy3fy0OXF3Os9maKpzR7BxT5s8",
-  authDomain: "impodoc-c12db.firebaseapp.com",
-  projectId: "impodoc-c12db",
-  storageBucket: "impodoc-c12db.firebasestorage.app",
-  messagingSenderId: "866475571341",
-  appId: "1:866475571341:web:c6510d03ae5cd7d11a8ac4",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
